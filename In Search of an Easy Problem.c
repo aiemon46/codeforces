28765ce0fe1a3f3e2fc26bf836fd03d26b096209
emon <ahammed.ibrahim.emon@gmail.com>
2022-09-04 21:36:01 +0600

    isoae problem add

---
+++ In Search of an Easy Problem.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0,t;
+    scanf("%d",&t);
+    while(t--){
+        
+        scanf("%d",&a);
+        if(a==1){
+        c=1;
+        }
+    }
+        if (c==1){
+                printf("HARD");
+        }
+        else{ 
+            printf("EASY");
+        }
+    
+}
